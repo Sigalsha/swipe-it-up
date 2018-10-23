39ac7b5f968fe7820cb84aa6ae1cd31dc61b77be
Hilla Meir <hillameir85@gmail.com>
2018-10-23 13:00:17 +0300

    commit 3

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'mobx-react';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
+import store from './js/store/GameStore';
+console.log(store)
+
+window.store = store;
+
+ReactDOM.render(<Provider store={store}>
+                <App />
+                </Provider>, document.getElementById('root'));
+    
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
+    
---
+++ src/js/store/GameStore.js
@@ -0,0 +1,13 @@
+import { observable } from "mobx";
+
+class GameStore {
+    @observable games = [];
+    @observable users = [{id:1,name:'user-1'},{id:2,name:'user-2'},{id:3,name:'user-3'}];
+ 
+    addUser(id ,name) {
+	this.users.push({ id, name });
+    }
+}
+
+const store = new GameStore();
+export default store;
