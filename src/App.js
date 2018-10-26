30b94495d6f3f17f82394d43e0c2bb33092c2859
Sigalsha <sigal.shalit.manor@gmail.com>
2018-10-26 18:59:37 +0300

    testing component positioning

--- src/App.js
+++ src/App.js
@@ -1,65 +1,70 @@
 import React, { Component } from 'react';
 import './App.css';
+import Touch from './touch'
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
-    this.state ={
-      clientStart: {x: "", y: ""} ,
-      clientEnd: {x: "", y: ""},
+    this.state = {
+      clientStart: { x: "", y: "" },
+      clientEnd: { x: "", y: "" },
       distance: null
     }
   }
 
-  go = (e) => {
-    let xStart = e.clientX
-    let yStart = e.clientY
-    console.log(xStart , yStart)
-    this.setState({clientStart: {x: xStart,y: yStart}})
-  }
+  // go = (e) => {
+  //   let xStart = e.clientX
+  //   let yStart = e.clientY
+  //   console.log(xStart, yStart)
+  //   this.setState({ clientStart: { x: xStart, y: yStart } })
+  // }
 
 
   stop = (e) => {
     let xEnd = e.clientX
     let yEnd = e.clientY
-    console.log(xEnd , yEnd)
-    this.setState({clientStart: {x: xEnd,y: yEnd}})
+    console.log(xEnd, yEnd)
+    this.setState({ clientStart: { x: xEnd, y: yEnd } })
   }
 
-  length = () => {
-    let clientStart = this.state.clientStart
-    let clientEnd = this.state.clientEnd
-    let xGap = clientStart.x - clientEnd.x
-    let yGap = clientStart.y - clientEnd.y
-    let x_square = Math.pow(xGap, 2)
-    let y_square = Math.pow(yGap, 2)
-    let distance = Math.sqrt(x_square + y_square)
-    console.log(distance)
-    this.setState({distance: distance})
-  }
+  // length = () => {
+  //   let clientStart = this.state.clientStart
+  //   let clientEnd = this.state.clientEnd
+  //   let xGap = clientStart.x - clientEnd.x
+  //   let yGap = clientStart.y - clientEnd.y
+  //   let x_square = Math.pow(xGap, 2)
+  //   let y_square = Math.pow(yGap, 2)
+  //   let distance = Math.sqrt(x_square + y_square)
+  //   console.log(distance)
+  //   this.setState({ distance: distance })
+  // }
 
   render() {
     return (
-      <div>
-      <div onDragStart={this.go} onDragEnd={this.stop} style={{ height: 50, width: 50, backgroundColor: 'gold' }}>Hello</div>
-      <button onClick={this.length}>click</button>
-      {this.state.distance}
+      <div onMouseDown={this.stop}>
+        {/* <div onClick={this.go} onDragEnd={this.stop} style={{ height: 50, width: 50, backgroundColor: 'gold' }}>Hello</div>
+        <button onClick={this.length}>click</button> */}
+        {this.state.distance}
+        <Touch />
+        <div style={{position: 'absolute', top: 20, left: 373, backgroundColor: 'green', height: 20, width: 20, color: 'white'}}>1</div>
+        <div style={{position: 'absolute', top: 24, left: 296, backgroundColor: 'green', height: 20, width: 20, color: 'white'}}>2</div>
+        <div style={{position: 'absolute', top: 61, left: 344, backgroundColor: 'green', height: 20, width: 20, color: 'white'}}>3</div>
+
       </div>
     )
   }
 }
 
 
-// x_traget_square = pow(x_target - x_end, 2)
-// y_traget_square = pow(y_target - y_end, 2)
-// distance = sqrt(x_target_square + y_target_square)
+
+// x_traget_square = Math.pow(x_target - x_end, 2)
+// y_traget_square = Math.pow(y_target - y_end, 2)
+// distance = Math.sqrt(x_target_square + y_target_square)
 
 // x_target, y_target is the position of your target.
 // x_end, y_end is the end position of the swipe.
 // I assume the ball position and the start position of the swipe are the same
 
-//(22, 43)
-//(23, 0)
 
 
 export default App;
--- src/touch.js
+++ src/touch.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
- 
+import ReactDOM from 'react-dom';
+import './touch.css'
+
+
 class Touch extends Component {
- 
-  swiping = () => {
-    alert("You're Swiping...")
+
+
+  constructor() {
+    super()
+    this.state = {
+      divPos: { x: "", y: "" },
+      clientEnd: { x: "", y: "" },
+      distance: null
+    }
   }
- 
+
+  componentDidMount() {
+    let rect = ReactDOM.findDOMNode(this)
+      .getBoundingClientRect()
+    console.log(rect.x, rect.y)
+    this.setState({ divPos: { x: rect.x, y: rect.y } })
+  }
+
+  stop = (e) => {
+    let xEnd = e.clientX
+    let yEnd = e.clientY
+    console.log(xEnd, yEnd)
+    let xCenter = this.state.divPos.x + 25;
+    let yCenter = this.state.divPos.y + 25;
+    let xSum = (xCenter - xEnd) * (xCenter - xEnd);
+    let ySum = (yCenter - yEnd) * (yCenter - yEnd)
+    let distance = Math.sqrt(xSum + ySum)
+    this.setState({ 
+    clientEnd: { x: xEnd, y: yEnd },
+    distance: distance  }, ()=> console.log(distance))
+    
+  }
+
+
+
+
   render() {
     return (
-      <div>
-          You can swipe here!
+      <div className="targetRangeDiv" onMouseDown={this.stop}>
+        {/* <div className="targetRangeDiv" onMouseLeave={this.stop}> */}
+        {/* <div className="targetDiv" ></div> */}
+
+
+        {/* <button onClick={this.divPosition}>click</button> */}
+
+        {/* {this.state.divPos}
+        {this.state.clientEnd} */}
       </div>
     )
   }
 }
+
+
 
 export default Touch;
