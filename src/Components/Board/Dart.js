f1ca3e34135e605ffdaf9630b11f2c8d62d5498b
Sigalsha <sigal.shalit.manor@gmail.com>
2018-10-28 10:42:08 +0200

    commit branch to exit it

--- src/Components/Board/Dart.js
+++ src/Components/Board/Dart.js
@@ -27,29 +27,29 @@ class Dart extends Component {
     let rect = ReactDOM.findDOMNode(this)
       .getBoundingClientRect()
     console.log(rect.x, rect.y)
-    this.iconPos.x = rect.x + 25 //finding the div's center
-    this.iconPos.y = rect.y + 25 //finding the div's center
+    this.iconPos.x = await rect.x + 25 //finding the div's center
+    this.iconPos.y = await rect.y + 25 //finding the div's center
     console.log(this.iconPos.x, this.iconPos.y)
-    await this.props.addPlayerIconPos(this.iconPos.x, this.iconPos.y) //send to store
+    this.props.addPlayerIconPos(this.iconPos.x, this.iconPos.y) //send to store
   }
 
   @action start = async (e) => {
     let xStart = e.clientX; //where the player's first touch on screen
     let yStart = e.clientY;
     console.log(xStart, yStart);
-    this.clientStart.x = xStart;
-    this.clientStart.y = yStart;
+    this.clientStart.x = await xStart;
+    this.clientStart.y = await yStart;
     console.log(this.clientStart.x, this.clientStart.y)
-    await this.props.checkStartPoint(this.clientStart.x, this.clientStart.y) //send to store
+    this.props.checkStartPoint(this.clientStart.x, this.clientStart.y) //send to store
   }
 
   @action stop = async (e) => {
     let xEnd = e.clientX
     let yEnd = e.clientY
     console.log(xEnd, yEnd)
-    this.clientEnd.x = xEnd;
-    this.clientEnd.y = yEnd;
-    await this.props.addShot(this.clientEnd.x, this.clientEnd.y)
+    this.clientEnd.x = await xEnd;
+    this.clientEnd.y = await yEnd;
+    this.props.addShot(this.clientEnd.x, this.clientEnd.y)
     //sending shot's (x,y) to the store 
 
   }
--- src/js/store/GameStore.js
+++ src/js/store/GameStore.js
@@ -4,33 +4,37 @@ class GameStore {
     @observable gameStatus = 'pending';
     @observable games = [];
     @observable users = [{ id: 1, name: 'user-1' }, { id: 2, name: 'user-2' }, { id: 3, name: 'user-3' }];
-    @observable target = { x: "", y: '' } //should get the target (x,y)
-    @observable playerIcon = { x: "", y: ""} //should get the player's icon (x,y)
-    @observable shot = { userName: "", startPoint: false, x: "", y: "" } 
+    target = { x: "", y: '' } //should get the target (x,y)
+    playerIcon = { x: "", y: ""} //should get the player's icon (x,y)
+    shot = { userName: "", startPoint: false, x: "", y: "" } 
     //shot => include username, startpoint(if player touched close to the startIcon),
     //and the shot (x,y)
 
-    @observable distanceTemp = {} //only distance from one shot to the target
-    @observable allDistances = [] //all the distances between the shots and the targets 
+    distanceTemp = {} //only distance from one shot to the target
+    allDistances = [] //all the distances between the shots and the targets 
+    score = 0
 
     @action addUser(id, name) {
         this.users.push({ id, name });
     }
 
     @action async addTargerPos(x, y) {
-        let targetObj = await {x, y}
+        let targetObj =  await {x: x, y: y}
+        console.log(targetObj)
         this.target.x = targetObj.x;
         this.target.y = targetObj.y;
     } //get the targetDiv's (x,y) from TargetTransperent component
 
     @action async addPlayerIconPos(x,y) {
-        let iconObj = await {x, y}
+        let iconObj = await {x: x, y: y}
+        console.log(iconObj)
         this.playerIcon.x = iconObj.x
         this.playerIcon.y = iconObj.y
     }
 
     @action async checkStartPoint(x, y) {
-        let startObj = await {x,y}
+        let startObj = await {x: x, y: y}
+        console.log(startObj)
       if ( (startObj.x <= this.playerIcon.x + 50) && (startObj.y <= this.playerIcon.y + 50) ) {
           this.shot.startPoint = true;
       } 
@@ -39,9 +43,10 @@ class GameStore {
 
 
     @action async addShot(x, y) {
-        let shotObj = await {x,y}
+        let shotObj = await {x: x, y: y}
         this.shot.x = shotObj.x;
         this.shot.y = shotObj.y;
+        //send the shot through socket.io
     } //get the shot's (x,y) from Dart component
 
     getDistance() {
@@ -56,6 +61,16 @@ class GameStore {
 
     getSum (total, num) {
         return total + num;
+    }
+
+    calculateScore () {
+        let distance = {...this.distanceTemp};
+        let shot = {...this.shot}
+        let score = this.score
+        if (!shot.startPoint) {
+
+
+        }
     }
 
     getScore() {
